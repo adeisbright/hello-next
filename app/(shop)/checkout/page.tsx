00994761f31163f304d57303ae934e033a611ad0
Adeleke Bright <bigjaratechnologies@gmail.com>
2023-12-27 05:17:58 +0100

    Add Route Groups

---
+++ app/(shop)/checkout/page.tsx
@@ -0,0 +1,9 @@
+import { Fragment } from "react"
+const Page = () => {
+    return (
+        <Fragment>
+            <h1>Checkout Page</h1> 
+        </Fragment>
+    )
+}
+export default Page 
---
+++ app/blog/[slug]/page.tsx
@@ -0,0 +1,4 @@
+const Page = ({ params }: { params: { slug: string } }) => {
+    return <div>My Post: {params.slug}</div>
+}
+export default Page 
